9ce24ac90a50fffcb3ac30470474471772ffd724
Tim Littlemore <tim@tjl2.com>
2014-04-28 17:44:03 +0100

    Just pretty print the fucker

--- hook-receiver.rb
+++ hook-receiver.rb
@@ -1,24 +1,13 @@
 # encoding: UTF-8
 require 'sinatra'
 require 'json'
+require 'pp'
 
 get '/' do
   "POST something to me…"
 end
 
 post '/' do
-  @payload = JSON.parse(params[:payload])
-  logger.info(parse_payload)
-  "I got this stuff: <br />#{parse_payload}"
-end
-
-private
-def parse_payload(payload=@payload)
-  payload.each do |key, value|
-    if value.is_array?
-      parse_payload(value)
-    else
-      puts "[#{key}] => #{value}"
-    end
-  end
+  payload = JSON.parse(params[:payload])
+  logger.info(pp payload)
 end
